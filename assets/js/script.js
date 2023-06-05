d52259766b1d3669afe24dcce20e24a6cb7becf0
IanPomaz <128009980+IanPomaz@users.noreply.github.com>
2023-06-05 19:08:11 -0400

    finish the buildString function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,34 @@
-var apiKey = "1fb9d06f";
+// var apiKey = "http://www.omdbapi.com/?s=thor&apikey=1fb9d06f";
 
-function getApi() {
+  function buildString() {
+    //pulls the input value 
+    let inputValue = document.getElementById("searchbox").value
+    var urlString = "http://www.omdbapi.com/?s="
+    var apiKey = "&apikey=1fb9d06f"
+
+    //concat all the variables together in the correct order
+    var buildURL = urlString + inputValue + apiKey
+                  //"http://www.omdbapi.com/?s=thor&apikey=1fb9d06f"
+    console.log(buildURL)    
+    
+    return buildURL
+     
+  }
+
+  
+  
+
+  // function getInputValue() {
+  //   let inputValue = document.getElementById("searchbox").value
+  //   // console.log("input value from getInputValue func")
+  //   console.log(inputValue)
+  //   buildString(inputValue)
+  //   return inputValue
+  // }
+
+  function getApi() {
     // Insert the API url to get a list of your repos
-    var requestUrl = 'http://www.omdbapi.com/?s=thor&apikey=1fb9d06f';
+    var requestUrl = (apiKey);
   
     fetch(requestUrl)
       .then(function (response) {
@@ -14,7 +40,6 @@ function getApi() {
         });
   }
 
-
-getApi()
-
+// var apiKey = buildString()
+// getApi()
 
