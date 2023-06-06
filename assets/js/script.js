a68746d47464dde830524e8fcebe0e46277bc3c9
IanPomaz <128009980+IanPomaz@users.noreply.github.com>
2023-06-05 20:34:07 -0400

    added event listener for getApi function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// var apiKey = "http://www.omdbapi.com/?s=thor&apikey=1fb9d06f";
+
 
   function buildString() {
     //pulls the input value 
@@ -9,26 +9,19 @@
     //concat all the variables together in the correct order
     var buildURL = urlString + inputValue + apiKey
                   //"http://www.omdbapi.com/?s=thor&apikey=1fb9d06f"
-    console.log(buildURL)    
+    console.log(buildURL)
     
     return buildURL
      
   }
 
-  
-  
 
-  // function getInputValue() {
-  //   let inputValue = document.getElementById("searchbox").value
-  //   // console.log("input value from getInputValue func")
-  //   console.log(inputValue)
-  //   buildString(inputValue)
-  //   return inputValue
-  // }
+
+
 
   function getApi() {
     // Insert the API url to get a list of your repos
-    var requestUrl = (apiKey);
+    var requestUrl = buildString();
   
     fetch(requestUrl)
       .then(function (response) {
@@ -38,8 +31,20 @@
         //looping over the fetch response and inserting the URL of your repos into a list
         console.log(data)
         });
+      
   }
 
+document.getElementById("submit").addEventListener("click",getApi)
+
 // var apiKey = buildString()
-// getApi()
+
+
+
+  // function getInputValue() {
+  //   let inputValue = document.getElementById("searchbox").value
+  //   // console.log("input value from getInputValue func")
+  //   console.log(inputValue)
+  //   buildString(inputValue)
+  //   return inputValue
+  // }
 
